364eb17ae56339747808b9609cc74ed441764482
Vagrant Default User <vagrant@debian-jessie.vagrantup.com>
2016-07-05 21:52:34 +0200

    Add package.json, .gitignore, some cleanup

--- cmds/abi.js
+++ cmds/abi.js
@@ -11,7 +11,5 @@ exports.builder = {
 exports.handler = function (argv) {
   util.getContract(argv.file).then(function(info) {
     console.log(JSON.stringify(info.abi));
-  }).catch(util.printException).finally(function() {
-    process.exit();
-  });
+  }).catch(util.printException);
 }
